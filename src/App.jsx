a7eba74eb78bc90381dc272f02428b353b630d3a
Johnny <shenjiaming2003@gmail.com>
2024-01-04 19:31:10 +0800

    switched to Hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import NavBar from "./components/navbar";
 import Calculator from "./components/calculator";
 import About from "./components/about"
 import './App.css'
-import { Router, Route, BrowserRouter, Routes} from "react-router-dom";
+import { Router, Route, HashRouter, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <NavBar/>
             <Routes>
                 <Route path="/about" element={<About/>} />
-                <Route exact path="/home" element={<Calculator/>} />
+                <Route path="/home" element={<Calculator/>} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 export default App
